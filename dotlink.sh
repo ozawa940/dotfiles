0eae6786b71b06d83ed2ac6fa30a25923638632a
ozawa <d.ozawa940@gmail.com>
2016-11-19 08:30:28 +0000

    add zprezto

--- dotlink.sh
+++ dotlink.sh
@@ -1,5 +1,14 @@
 #!/bin/zsh
 mkdir -p ~/.config/nvim/
+#install zprezto
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+
+
+#link
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.zpreztorc ~/.zpreztorc
